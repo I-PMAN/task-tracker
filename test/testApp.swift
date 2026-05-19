acbf05e0492894ba792c54ed68fac90d0e32f0c1
I-PMAN <hero.parth@gmail.com>
2026-05-19 22:21:28 +0530

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Parth Bherwani on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
